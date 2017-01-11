e2f0195a9fdb6ded00dc97be5aa05527bdcf23da
Hexzilla <hexzilla256@gmail.com>
2017-01-11 09:17:59 +0300

    tests

--- tests/Board.test.js
+++ tests/Board.test.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Board from '../components/lib/Board'
+import Board from '../src/components/Board'
 
 describe('<Board/>', () => {
   it('snapshot test', () => {
+    const data = {lanes: [{id: 'Lane1', title: 'Lane1', cards: []}]}
     const component = renderer.create(
-      <Board key='board1'>
-        Many boards here
-      </Board>
+      <Board key='board1' data={data} />
     )
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
