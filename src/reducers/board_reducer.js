c91d37e83943cc3e41077211045214bf17ec4976
Hexzilla <hexzilla256@gmail.com>
2017-01-13 08:29:59 +0300

    board reducer to move card

--- src/reducers/board_reducer.js
+++ src/reducers/board_reducer.js
@@ -2,19 +2,50 @@ const appendCardsToLane = (state, {laneId, newCards}) => {
   const lanes = state.lanes.map((lane) => {
     if (lane.id === laneId) {
       lane.cards = [...lane.cards, ...newCards]
-      return lane
     }
+    return lane
   })
   return {...state, ...lanes}
 }
 
-const boardReducer = (state = {lanes: []}, action) => {
+const appendCardToLane = (state, {laneId, card}) => {
+  return appendCardsToLane(state, {laneId: laneId, newCards: [card]})
+}
 
+const removeCardFromLane = (state, {laneId, cardId}) => {
+  const lanes = state.lanes.map((lane) => {
+    if (lane.id === laneId) {
+      lane.cards = lane.cards.filter((card) => card.id !== cardId)
+    }
+    return lane
+  })
+  return {...state, ...lanes}
+}
+
+const moveCardAcrossLanes = (state, {fromLaneId, toLaneId, cardId}) => {
+  let cardToMove = null
+  const interimLanes = state.lanes.map((lane) => {
+    if (lane.id === fromLaneId) {
+      cardToMove = lane.cards.find((card) => card.id === cardId)
+      lane.cards = lane.cards.filter((card) => card.id !== cardId)
+    }
+    return lane
+  })
+  return appendCardToLane({lanes: interimLanes}, {laneId: toLaneId, card: cardToMove})
+}
+
+const boardReducer = (state = {lanes: []}, action) => {
   switch (action.type) {
     case 'LOAD_BOARD':
       return action.payload
     case 'UPDATE_LANE':
       return appendCardsToLane(state, action.payload)
+    case 'ADD_CARD':
+      return appendCardToLane(state, action.payload)
+    case 'REMOVE_CARD':
+      return removeCardFromLane(state, action.payload)
+    case 'MOVE_CARD':
+      return moveCardAcrossLanes(state, action.payload)
     default:
       return state
   }
