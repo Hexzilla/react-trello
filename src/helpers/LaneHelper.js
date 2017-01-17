dee1011c06403c546bce9c1a6a5bc40da78875bd
Hexzilla <hexzilla256@gmail.com>
2017-01-17 08:43:47 +0300

    update reducer and add helpers

---
+++ src/helpers/LaneHelper.js
@@ -0,0 +1,50 @@
+const LaneHelper = {
+
+  appendCardsToLane: (state, {laneId, newCards}) => {
+    const lanes = state.lanes.map((lane) => {
+      if (lane.id === laneId) {
+        lane.cards = [...lane.cards, ...newCards]
+      }
+      return lane
+    })
+    return {...state, ...lanes}
+  },
+
+  appendCardToLane: (state, {laneId, card}) => {
+    return LaneHelper.appendCardsToLane(state, {laneId: laneId, newCards: [card]})
+  },
+
+  removeCardFromLane: (state, {laneId, cardId}) => {
+    const lanes = state.lanes.map((lane) => {
+      if (lane.id === laneId) {
+        lane.cards = lane.cards.filter((card) => card.id !== cardId)
+      }
+      return lane
+    })
+    return {...state, ...lanes}
+  },
+
+  moveCardAcrossLanes: (state, {fromLaneId, toLaneId, cardId}) => {
+    let cardToMove = null
+    const interimLanes = state.lanes.map((lane) => {
+      if (lane.id === fromLaneId) {
+        cardToMove = lane.cards.find((card) => card.id === cardId)
+        lane.cards = lane.cards.filter((card) => card.id !== cardId)
+      }
+      return lane
+    })
+    return LaneHelper.appendCardToLane({lanes: interimLanes}, {laneId: toLaneId, card: cardToMove})
+  },
+
+  updateCardsForLane: (state, {laneId, cards}) => {
+    const lanes = state.lanes.map((lane) => {
+      if (lane.id === laneId) {
+        lane.cards = cards
+      }
+      return lane
+    })
+    return {...state, ...lanes}
+  }
+}
+
+export default LaneHelper
