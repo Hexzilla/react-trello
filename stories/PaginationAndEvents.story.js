f04771d9060bd9ee1b56a76275dde44434956187
Hexzilla <hexzilla256@gmail.com>
2017-01-20 09:22:30 +0300

    pagination and event

--- stories/PaginationAndEvents.story.js
+++ stories/PaginationAndEvents.story.js
@@ -40,15 +40,12 @@ class BoardWrapper extends Component {
   }
 
   refreshCards = () => {
-    const {data} = this.state
-    data.lanes = [{
+    eventBus.publish({type: 'REFRESH_BOARD', data: {lanes: [{
       id: 'Lane1',
       title: 'Changed Lane',
       cards: []
-    }]
-    this.setState({data: data})
+    }]}})
   }
-
 
   paginate = (requestedPage, laneId) => {
     let newCards = generateCards(requestedPage);
@@ -91,3 +88,4 @@ storiesOf('react-trello', module)
 
       return <BoardWrapper data={data}/>
     })
+
